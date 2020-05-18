2cd88692739131de34dcd2824425e610b2b088b5
Golbarg200 <pepi_krastev@abv.bg>
2020-05-18 10:24:25 +0300

    Added Basic CRUD

---
+++ C#/Tech Module/BasicCrud-Exercise/TeisterMask/Data/TeisterMaskDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeisterMask.Models;
+
+namespace TeisterMask.Data
+{
+    public class TeisterMaskDbContext:DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-4PK0GK7\SQLEXPRESS;Database=TeisterMaskDb;Integrated Security=True;");
+        }
+    }
+}
---
+++ C#/Tech Module/BasicCrud-Lab/ToDoList/Data/ToDoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoList.Data
+{
+    public class ToDoDbContext:DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=ToDoListDb;Integrated Security=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
