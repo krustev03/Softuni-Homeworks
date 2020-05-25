0212069857b8b238e22c7db5146ecf938603f339
Golbarg200 <pepi_krastev@abv.bg>
2020-05-25 14:04:43 +0300

    Added Defining Classes

---
+++ C#/C# Advanced/Advanced/DefiningClasses-Exercise/SoftUniParking/Parking.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.cars = new Dictionary<string, Car>();
+            this.capacity = capacity;
+        }
+
+        public List<Car> Cars { get; }
+
+        public int Capacity { get; }
+
+        public int Count => cars.Count;
+
+        public string AddCar(Car car)
+        {
+            if (cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (cars.Count == capacity)
+            {
+                return "Parking is full!";
+            }
+           
+            cars.Add(car.RegistrationNumber, car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+        }
+
+        public string RemoveCar(string registrationNumber)
+        {
+            if (!cars.ContainsKey(registrationNumber))
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            
+            cars.Remove(registrationNumber);
+            return $"Successfully removed {registrationNumber}";
+            
+        }
+
+        public Car GetCar(string registrationNumber)
+        {
+            Car car = null;
+
+            if (cars.ContainsKey(registrationNumber))
+            {
+                car = cars[registrationNumber];
+            }
+
+            return car;
+        }
+
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in registrationNumbers)
+            {
+                RemoveCar(registrationNumber);
+            }
+        }
+    }
+}
---
+++ C#/C# Advanced/Advanced/DefiningClasses-Lab/CarManufacturer/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            bool canContinue = this.FuelQuantity - (distance * this.FuelConsumption) >= 0;
+
+            if (canContinue)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoIAm()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.AppendLine($"Fuel: {this.FuelQuantity:F2}L");
+            return result.ToString();
+        }
+
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year,
+                   double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        private Engine engine;
+        private Tire tires;
+
+        public Engine Engine { get; set; }
+        public Tire[] Tire { get; set; }
+
+        public Car(string make, string model, int year,
+        double fuelQuantity, double fuelConsumption, Engine engine,
+        Tire[] tires) : this(make, model, year, fuelQuantity, fuelConsumption)
+
+        {
+
+            this.Engine = engine;
+
+            this.Tire = tires;
+
+        }
+    }
+}
