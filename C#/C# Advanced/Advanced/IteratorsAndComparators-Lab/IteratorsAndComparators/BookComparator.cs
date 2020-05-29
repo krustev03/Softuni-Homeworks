d6d3d034f45329b708c6387471b7d739f25470c0
Golbarg200 <pepi_krastev@abv.bg>
2020-05-29 07:53:33 +0300

    Added Iterators And Comparators

---
+++ C#/C# Advanced/Advanced/IteratorsAndComparators-Lab/IteratorsAndComparators/BookComparator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book first, Book second)
+        {
+            int result = first.Title.CompareTo(second.Title);
+
+            if (result == 0)
+            {
+                return second.Year - first.Year;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/C# Advanced/Advanced/IteratorsAndComparators-Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params string[] books)
+        {
+            this.books = new SortedSet<Book>(new BookComparator());
+        }
+
+        public void AddBook(Book book)
+        {
+            this.books.Add(book);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            foreach (var book in this.books)
+            {
+                yield return book;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
