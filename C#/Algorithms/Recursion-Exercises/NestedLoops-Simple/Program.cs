decb9c100ef15cfb4267106a309a3e6c835e4461
Golbarg200 <pepi_krastev@abv.bg>
2021-02-11 18:16:09 +0200

    Recursion added

---
+++ C#/Algorithms/Recursion-Exercises/NestedLoops-Simple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NestedLoops_Simple
+{
+    class Program
+    {
+        static int counter = 1;
+
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            PrintNestedLoops(n - 1);
+        }
+
+        static void PrintNestedLoops(int n)
+        {
+            if (n < 0)
+            {
+                return;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+
+            PrintNestedLoops(n - 1);
+        }
+    }
+}
---
+++ C#/Algorithms/Recursion-Exercises/PrintNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PrintNumbers
+{
+    class Program
+    {
+        static int counter = 1;
+
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("k = ");
+            int k = int.Parse(Console.ReadLine());
+
+            PrintNumbers(n, k);
+        }
+
+        static void PrintNumbers(int n, int k)
+        {
+            for (int i = counter; i <= n; i++)
+            {
+                if (counter == n && i == n)
+                {
+                    Console.Write($"({counter} {i})");
+                }
+                else
+                {
+                    Console.Write($"({counter} {i}), ");
+                }
+            }
+
+            counter++;
+
+            if (counter <= n)
+            {
+                PrintNumbers(n, k);
+            }
+
+            return;
+        }
+    }
+}
